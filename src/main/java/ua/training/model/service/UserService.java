facb01d62bdc8b8f6a002fe498fb18676f807a4b
= <=>
2020-02-05 15:28:34 +0200

    UserService

---
+++ src/main/java/ua/training/model/service/UserService.java
@@ -0,0 +1,27 @@
+package ua.training.model.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import ua.training.model.dao.DaoFactory;
+import ua.training.model.dao.UserDao;
+import ua.training.model.entity.User;
+
+import java.util.Optional;
+
+public class UserService {
+
+    DaoFactory daoFactory = DaoFactory.getInstance();
+
+    public Optional<User> getUserByEmailAndPassword(String email, String password) {
+        Optional<User> result = Optional.empty();
+        try (UserDao userDao = daoFactory.createUserDao()) {
+            result = userDao.findByEmail(email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean isPassCorrect(String inputPass, String userPass) {
+        return BCrypt.checkpw(inputPass, userPass);
+    }
+}
