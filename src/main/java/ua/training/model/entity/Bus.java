98d8518a59c5fbfc17a2de84a56ccc7c9a976360
= <=>
2020-02-11 15:30:55 +0200

    1_updated entities

--- src/main/java/ua/training/model/entity/Bus.java
+++ src/main/java/ua/training/model/entity/Bus.java
@@ -2,16 +2,13 @@ package ua.training.model.entity;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 public class Bus {
     private int id;
     private String mark;
     private String licensePlate;
-    private Boolean free;
 
     private List<User> drivers;
-    private List<Route> routeList;
 
     public Bus() {
     }
@@ -20,15 +17,11 @@ public class Bus {
             int id,
             String mark,
             String licensePlate,
-            Boolean free,
-            List<User> drivers,
-            List<Route> routeList) {
+            List<User> drivers) {
         this.id = id;
         this.mark = mark;
         this.licensePlate = licensePlate;
-        this.free = free;
         this.drivers = drivers;
-        this.routeList = routeList;
     }
 
     public int getId() {
@@ -55,28 +48,12 @@ public class Bus {
         this.licensePlate = licensePlate;
     }
 
-    public Boolean getFree() {
-        return free;
-    }
-
-    public void setFree(Boolean free) {
-        this.free = free;
-    }
-
     public List<User> getDrivers() {
         return drivers;
     }
 
     public void setDrivers(List<User> drivers) {
         this.drivers = drivers;
-    }
-
-    public List<Route> getRouteList() {
-        return routeList;
-    }
-
-    public void setRouteList(List<Route> routeList) {
-        this.routeList = routeList;
     }
 
     @Override
@@ -86,72 +63,12 @@ public class Bus {
         Bus bus = (Bus) o;
         return Objects.equals(id, bus.id) &&
                 Objects.equals(mark, bus.mark) &&
-                Objects.equals(licensePlate, bus.licensePlate) &&
-                Objects.equals(free, bus.free);
+                Objects.equals(licensePlate, bus.licensePlate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, mark, licensePlate, free);
+        return Objects.hash(id, mark, licensePlate);
     }
 
-    public static class Builder {
-        private Bus busBuilder;
-
-        public Builder() {
-            busBuilder = new Bus();
-        }
-
-        private Long id;
-        private String mark;
-        private String licensePlate;
-        private Boolean free;
-
-        private Set<User> drivers;
-        private Set<Route> routeList;
-
-        public Builder id(int id) {
-            busBuilder.id = id;
-            return this;
-        }
-
-        public Builder mark(String mark) {
-            busBuilder.mark = mark;
-            return this;
-        }
-
-        public Builder licensePlate(String licensePlate) {
-            busBuilder.licensePlate = licensePlate;
-            return this;
-        }
-
-        public Builder free(Boolean free) {
-            busBuilder.free = free;
-            return this;
-        }
-
-        public Builder drivers(List<User> drivers) {
-            busBuilder.drivers = drivers;
-            return this;
-        }
-
-        public Builder driver(User driver) {
-            busBuilder.drivers.add(driver);
-            return this;
-        }
-
-        public Builder routeList(List<Route> routeList) {
-            busBuilder.routeList = routeList;
-            return this;
-        }
-
-        public Builder route(Route route) {
-            busBuilder.routeList.add(route);
-            return this;
-        }
-
-        public Bus build() {
-            return busBuilder;
-        }
-    }
 }
